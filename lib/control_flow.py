a586ee15e9f06c76af3aa3c9fd4bdd8c221ed9f3
Iqra <iqra.ali@student.moringaschool.com>
2023-09-14 08:48:38 +0300

    conditionals

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -12,12 +12,12 @@ def hows_the_weather(temperature):
     # your code here
     if temperature < 40:
         return "It's brisk out there!"
-    elif temperature >= 40 and temperature <=65:
-        return "It's a little chilly out here!"
+    elif 40 <= temperature <=65:
+        return "It's a little chilly out there!"
     elif temperature > 85:
         return "It's too dang hot out there!"
     else:
-        return "It's hot out there!"
+        return "It's perfect out there!"
     pass
 
 def fizzbuzz(num):
